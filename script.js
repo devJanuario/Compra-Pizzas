db22b0c00ac0f5836cdb300d7354cad9f69ce387
Alysson De Oliveira Januário <155186045+AlyssInh0@users.noreply.github.com>
2024-05-09 18:57:42 -0300

    Atualizando o arquivo script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const c = (e) => document.querySelector(e)
+const cs = (e) => document.querySelectorAll(e)
+
 pizzaJson.map((item, index) => {
-    
+    let pizzaItem = c('.models .pizza-item').cloneNode(true) // Para clonar o item
+
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+
+    c('.pizza-area').append(pizzaItem)
 })
